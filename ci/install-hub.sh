dfdd73a50279aa7e884acf357a04efa94698ef6c
Bojan <dbojan@gmail.com>
2020-01-11 18:17:21 -0400

    fix hub links

--- ci/install-hub.sh
+++ ci/install-hub.sh
@@ -3,17 +3,17 @@
 set -ex
 case $1 in
   ubuntu*)
-    curl -LsSf https://github.com/github/hub/releases/download/v2.12.8/hub-linux-amd64-2.13.0.tgz -o hub.tgz
+    curl -LsSf https://github.com/github/hub/releases/download/v2.13.0/hub-linux-amd64-2.13.0.tgz -o hub.tgz
     mkdir hub
     tar -xzvf hub.tgz --strip=1 -C hub
     ;;
   macos*)
-    curl -LsSf https://github.com/github/hub/releases/download/v2.12.8/hub-darwin-amd64-2.13.0.tgz -o hub.tgz
+    curl -LsSf https://github.com/github/hub/releases/download/v2.13.0/hub-darwin-amd64-2.13.0.tgz -o hub.tgz
     mkdir hub
     tar -xzvf hub.tgz --strip=1 -C hub
     ;;
   windows*)
-    curl -LsSf https://github.com/github/hub/releases/download/v2.12.8/hub-windows-amd64-2.13.0.zip -o hub.zip
+    curl -LsSf https://github.com/github/hub/releases/download/v2.13.0/hub-windows-amd64-2.13.0.zip -o hub.zip
     7z x hub.zip -ohub
     ;;
   *)
