c022f2094e49177c22f0b29cd0049374f1c84742
Bojan <dbojan@gmail.com>
2020-01-18 16:43:12 -0400

    build: add cargo bin to build path

--- docs/build.sh
+++ docs/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/.cargo/bin/mdpage ./docs/examples/basic
-/.cargo/bin/mdpage ./docs/examples/sections
-/.cargo/bin/mdpage ./docs/examples/full_page --full-page --title "Full Page" --subtitle "Full page example"
-/.cargo/bin/mdpage ./docs/examples/config
-/.cargo/bin/mdpage ./docs/examples/single_index --title "Single page" --subtitle "Single main page"
-/.cargo/bin/mdpage ./docs/examples/single_page --title "Single page" --subtitle "Single content page"
-/.cargo/bin/mdpage ./docs/examples/config_dir
-/.cargo/bin/mdpage ./docs
+mdpage ./docs/examples/basic
+mdpage ./docs/examples/sections
+mdpage ./docs/examples/full_page --full-page --title "Full Page" --subtitle "Full page example"
+mdpage ./docs/examples/config
+mdpage ./docs/examples/single_index --title "Single page" --subtitle "Single main page"
+mdpage ./docs/examples/single_page --title "Single page" --subtitle "Single content page"
+mdpage ./docs/examples/config_dir
+mdpage ./docs
