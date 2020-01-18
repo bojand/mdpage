590220d15536328fb6a6122251c207dcd4e205d9
Bojan <dbojan@gmail.com>
2020-01-18 16:18:39 -0400

    build: Fix mdpage path in docs/build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./target/debug/mdpage ./docs/examples/basic
-./target/debug/mdpage ./docs/examples/sections
-./target/debug/mdpage ./docs/examples/full_page --full-page --title "Full Page" --subtitle "Full page example"
-./target/debug/mdpage ./docs/examples/config
-./target/debug/mdpage ./docs/examples/single_index --title "Single page" --subtitle "Single main page"
-./target/debug/mdpage ./docs/examples/single_page --title "Single page" --subtitle "Single content page"
-./target/debug/mdpage ./docs/examples/config_dir
-./target/debug/mdpage ./docs
+mdpage ./docs/examples/basic
+mdpage ./docs/examples/sections
+mdpage ./docs/examples/full_page --full-page --title "Full Page" --subtitle "Full page example"
+mdpage ./docs/examples/config
+mdpage ./docs/examples/single_index --title "Single page" --subtitle "Single main page"
+mdpage ./docs/examples/single_page --title "Single page" --subtitle "Single content page"
+mdpage ./docs/examples/config_dir
+mdpage ./docs
